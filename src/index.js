1c51612fbd5551340044501365072a2ca0a427df
Omar113650 <omarelhelaly520@gmail.com>
2026-01-31 01:35:04 +0200

    add public dir and deploy in vercel

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use(helmet());
 app.use(hpp());
