53e6aa2a1c3ae5756e9bf7613d15505894ddfc3a
Omar113650 <omarelhelaly520@gmail.com>
2026-01-31 00:41:18 +0200

    add some library security

--- src/index.js
+++ src/index.js
@@ -5,10 +5,40 @@ import { notfound, errorHandler } from "./middleware/error.js";
 import mongoSanitize from "express-mongo-sanitize";
 import statusMonitor from "express-status-monitor";
 import { globalSanitizer } from "./middleware/sanitization.js";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 dotenv.config({ path: ".env" });
 connectDB();
 
 const app = express();
+
+app.use(helmet());
+app.use(hpp());
+app.use(
+  cors({
+    origin: [
+      // "http://localhost:5173",
+    ],
+
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(morgan());
+
+app.use((req, res, next) => {
+  if (req.body) mongoSanitize.sanitize(req.body);
+  if (req.params) mongoSanitize.sanitize(req.params);
+  next();
+});
+
+app.use(statusMonitor());
+
 app.use(express.json());
 app.use(globalSanitizer);
 
@@ -24,7 +54,6 @@ import authRoutes from "./routes/AuthRoute.js";
 import logsRoutes from "./routes/LogsRoute.js";
 import taskRoutes from "./routes/TaskRoute.js";
 import dashboardRoutes from "./routes/DashboardRoute.js";
-
 
 app.get("/", (req, res) => res.send("Hello in vercel"));
 
